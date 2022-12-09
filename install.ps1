5a223f54d13356649f0dd376237cd5b2d649c84f
Sven Vanderwegen <sven@vbc.be>
2022-12-09 16:59:29 +0100

    Initial commit

---
+++ install.ps1
@@ -0,0 +1,14 @@
+#Requires -RunAsAdministrator
+
+Clear-Screen
+winget install -e --id Adobe.Acrobat.Reader.64-bit
+winget install -e --id EclipseAdoptium.Temurin.18.JRE
+winget install -e --id Mozilla.Firefox
+winget install -e --id Google.Chrome
+winget install -e --id Microsoft.Edge
+winget install -e --id Microsoft.OneDrive
+winget install -e --id Microsoft.Office
+winget install -e --id ZeroTier.ZeroTierOne
+winget install -e --id 7zip.7zip
+winget install -e --id BelgianGovernment.Belgium-eIDmiddleware
+winget install -e --id BelgianGovernment.eIDViewer
